f0324d0aaa4e73bc19d735c73e0834f2c9b78d25
Orkii <78535612+Orkii@users.noreply.github.com>
2023-12-09 17:13:00 +0500

    + Создание агентов из файла

--- JADE-bin-4.6.0/jade/lib/Helper/CfgReader.java
+++ JADE-bin-4.6.0/jade/lib/Helper/CfgReader.java
@@ -42,10 +42,20 @@ public class CfgReader {
                 StorageContainerName = str.replace("Storage:", "");
             }
         }
-    } 
-    private static void cfgShopsLoad(String temp){
-        System.out.println("Found ShopsM{");
 
+        ProfileImpl prof1 = new ProfileImpl(true);
+        ProfileImpl prof2 = new ProfileImpl(true);
+        ProfileImpl prof3 = new ProfileImpl(true);
+        prof1.setParameter(prof1.CONTAINER_NAME, ShopsContainerName);
+        prof2.setParameter(prof2.CONTAINER_NAME, CarsContainerName);
+        prof3.setParameter(prof3.CONTAINER_NAME, StorageContainerName);        
+        Runtime instance = Runtime.instance();
+        shopsContainer = instance.createAgentContainer(prof1);
+        carsContainer = instance.createAgentContainer(prof2);
+        storageContainer = instance.createAgentContainer(prof3);
+    } 
+    
+    private static void cfgCarLoad(String temp){
         while (temp.contains("Create{")){
             
             try {
@@ -54,34 +64,72 @@ public class CfgReader {
                 ex.printStackTrace();
             }
             
-
-            System.out.println("\nFound Create{");
             int q1 = temp.indexOf("Create{");
             int q2 = temp.indexOf("}Create")  + "}Create".length();
-            System.out.println("Create tempCreate");
-
-            System.out.println("q1 = " + q1 + " q2 = " + q2);
-            
+           
             String tempCreate = temp.substring(q1, q2);
-
-            System.out.println("tempCreate = " + tempCreate);
-
-            System.out.println("Create replace");
             temp = temp.replace(tempCreate, "");
-
-            System.out.println("Going to create a new Shop in container " + ShopsContainerName + " right now...");
-
 
             String[] subStr = tempCreate.split("\n");
 
-            System.out.println("subSTR = " + subStr);
+            for (String str : subStr) {
+                if (str.contains("Name:")){
+                    String agName = str.replace("Name:", "");
+                    try {
+                        carsContainer.createNewAgent(agName, "Agents.Car", null).start();
+                         
+                    } catch (StaleProxyException e) {
+                        System.out.println("Smth go WRON whil create Shop agent " + agName);
+                        e.printStackTrace();
+                    }
+                }
+            }       
+        }
+        System.out.println("Shop end");
+    } 
+    private static void cfgStorageLoad(String temp){
+        String[] subStr = temp.split("\n");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        for (String str : subStr) {
+            if (str.contains("Name:")){
+                if (str.contains("Name:")){
+                    String agName = str.replace("Name:", "");
+                    try {
+                        storageContainer.createNewAgent(agName, "Agents.Storage", null).start();
+                         
+                    } catch (StaleProxyException e) {
+                        System.out.println("Smth go WRON whil create Storage agent " + agName);
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    } 
+    private static void cfgShopsLoad(String temp){
+        while (temp.contains("Create{")){
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            
+            int q1 = temp.indexOf("Create{");
+            int q2 = temp.indexOf("}Create")  + "}Create".length();
+            
+            String tempCreate = temp.substring(q1, q2);
+            temp = temp.replace(tempCreate, "");
+
+            String[] subStr = tempCreate.split("\n");
 
             for (String str : subStr) {
                 if (str.contains("Name:")){
-                    System.out.println("Found Name:");
                     String agName = str.replace("Name:", "");
 
-                    System.out.println("AgName = " + agName);
                     try {
                         shopsContainer.createNewAgent(agName, "Agents.Shop", null).start();
                          
@@ -92,6 +140,7 @@ public class CfgReader {
                 }
             }       
         }
+        System.out.println("Shop end");
     }
 
 
@@ -109,35 +158,9 @@ public class CfgReader {
             int p1 = content.indexOf("ContainerNames{");
             int p2 = content.indexOf("}ContainerNames");
             String temp = content.substring(p1, p2);
-            content.replace(temp, "");
-
-            String[] subStr = temp.split("\n");
-
-            for (String str : subStr) {
-                if (str.contains("Shops:")){
-                    ShopsContainerName = str.replace("Shops:", "");
-                }
-                else if (str.contains("Cars:")){
-                    CarsContainerName = str.replace("Cars:", "");
-                }
-                else if (str.contains("Storage:")){
-                    StorageContainerName = str.replace("Storage:", "");
-                }
-            }
+            content = content.replace(temp, "");
             
-
-            ProfileImpl prof1 = new ProfileImpl(true);
-            ProfileImpl prof2 = new ProfileImpl(true);
-            ProfileImpl prof3 = new ProfileImpl(true);
-            prof1.setParameter(prof1.CONTAINER_NAME, ShopsContainerName);
-            prof2.setParameter(prof2.CONTAINER_NAME, CarsContainerName);
-            prof3.setParameter(prof3.CONTAINER_NAME, StorageContainerName);        
-            Runtime instance = Runtime.instance();
-            shopsContainer = instance.createAgentContainer(prof1);
-            carsContainer = instance.createAgentContainer(prof2);
-            storageContainer = instance.createAgentContainer(prof3);
-
-
+            cfgContainerLoad(temp);
         }
         
         System.out.println("Containers Done");
@@ -151,65 +174,26 @@ public class CfgReader {
             int p1 = content.indexOf("ShopsM{");
             int p2 = content.indexOf("}ShopsM");
             String temp = content.substring(p1, p2);
-            content.replace(temp, "");
+            content = content.replace(temp, "");
 
-            System.out.println("Found ShopsM{");
-
-            while (temp.contains("Create{")){
-                
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-                
-
-                System.out.println("\nFound Create{");
-                int q1 = temp.indexOf("Create{");
-                int q2 = temp.indexOf("}Create")  + "}Create".length();
-                System.out.println("Create tempCreate");
-
-                System.out.println("q1 = " + q1 + " q2 = " + q2);
-                
-                String tempCreate = temp.substring(q1, q2);
-
-                System.out.println("tempCreate = " + tempCreate);
-
-                System.out.println("Create replace");
-                temp = temp.replace(tempCreate, "");
-
-                System.out.println("Going to create a new Shop in container " + ShopsContainerName + " right now...");
-
-
-                String[] subStr = tempCreate.split("\n");
-
-                System.out.println("subSTR = " + subStr);
-
-                for (String str : subStr) {
-                    if (str.contains("Name:")){
-                        System.out.println("Found Name:");
-                        String agName = str.replace("Name:", "");
-
-                        System.out.println("AgName = " + agName);
-                        try {
-                            shopsContainer.createNewAgent(agName, "Agents.Shop", null).start();
-                             
-                        } catch (StaleProxyException e) {
-                            System.out.println("Smth go WRON whil create Shop agent " + agName);
-                            e.printStackTrace();
-                        }
-
-                    }
-                }
-                
-                //shopsContainer.acceptNewAgent(null, ca);
-                
-            }
-
-
+            cfgShopsLoad(temp);
         }
-    
-
+        {
+            System.out.println("Start storage");
+            int p1 = content.indexOf("StorageM{");
+            int p2 = content.indexOf("}StorageM");
+            String temp = content.substring(p1, p2);
+            content = content.replace(temp, "");
+            cfgStorageLoad(temp);
+        }
+        {
+            System.out.println("Start car");
+            int p1 = content.indexOf("CarM{");
+            int p2 = content.indexOf("}CarM");
+            String temp = content.substring(p1, p2);
+            content = content.replace(temp, "");
+            cfgCarLoad(temp);
+        }
 
 
     }
---
+++ JADE-bin-4.6.0/jade/lib/a.bat
@@ -0,0 +1,3 @@
+javac -d classes Agents/*.java
+cls
+java jade.Boot -container -agents Bob:Agents.Monitor
